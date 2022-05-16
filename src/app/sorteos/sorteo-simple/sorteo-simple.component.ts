8ac8d0797aca6a14a2fcf1902ef6c6de26c2f5e1
bienzobasjulian <bienzoj@gmail.com>
2022-05-16 17:41:30 +0200

    TODO: Sortear en sorteo simple

--- src/app/sorteos/sorteo-simple/sorteo-simple.component.ts
+++ src/app/sorteos/sorteo-simple/sorteo-simple.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sorteo-simple',
@@ -6,12 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sorteo-simple.component.css'],
 })
 export class SorteoSimpleComponent implements OnInit {
+
+  @ViewChild('inputNewParticipante') inputNewParticipante!: ElementRef; 
+  @ViewChild('inputNumPremios') inputNumPremios!: ElementRef;
   constructor() {}
 
   ngOnInit(): void {}
 
   participantes: string[] = [];
   newParticipante: string = '';
+  numPremios: number | null = null  ;
+  hayError : boolean = false;
+  mensajeError : string = '';
 
   addParticipante() {
     let valorParticipante = this.newParticipante;
@@ -27,5 +33,42 @@ export class SorteoSimpleComponent implements OnInit {
     this.newParticipante = '';
   }
 
+  eliminarParticipante(i : number){
+    this.participantes.splice(i, 1);
+  }
+
+  setFocusOnNewParticipante(){
+    this.inputNewParticipante.nativeElement.focus();
+  }
+
+  setFocusOnNumPremios(){
+    this.inputNumPremios.nativeElement.focus();
+  }
+
+  removeAllParticipantes(){
+    this.participantes = [] ;
+  }
+
+  sortear(){
+      if (this.numPremios == 0 || this.numPremios == null) {
+        //Si no se indica el nº de premios, se repartirán tantos premios como participantes haya.
+        this.numPremios = this.participantes.length;
+      }
+
+     //Si no hay participantes... 
+     if (this.participantes.length == 0){
+       this.hayError = true;
+       this.mensajeError = 'Por lo menos debe haber un participante';
+     }
+
+
+  }
+
+  //TODO: btnCopy
+
+  //TODO: Exportar / Descargar / Guardar
+
   
+
+
 }
